917cc9a4a382327b81105abd7b54011f29ddf63b
Cayde Xu <cayde.xu@oocl.com>
2019-07-12 15:55:16 +0800

    cayde: finished return fizzbuzz when convert given 15 times number.

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
 
     public String convert(int number){
+        if(number % 15 == 0){
+            return "fizzbuzz";
+        }
         if(number % 3 == 0){
             return "fizz";
         }
