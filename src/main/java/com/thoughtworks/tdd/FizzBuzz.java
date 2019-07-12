f4465a86e8b9b8fcf4f893c993e98d2406cfd4df
Cayde Xu <cayde.xu@oocl.com>
2019-07-12 15:54:43 +0800

    Revert "billy: finished return fizzbuzz when convert given 15 times number."
    This reverts commit d4a2b80f

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,9 +3,6 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
 
     public String convert(int number){
-        if(number % 15 == 0){
-            return "fizzbuzz";
-        }
         if(number % 3 == 0){
             return "fizz";
         }
